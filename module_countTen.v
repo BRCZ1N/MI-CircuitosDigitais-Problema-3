98ca2a74fbb227dde5e2465dd8b91bdb570101f9
Alexjr <88115079+Oguelo@users.noreply.github.com>
2024-01-05 18:42:55 -0300

    modulo contador 10

---
+++ module_countTen.v
@@ -0,0 +1,8 @@
+module and_gate_3_inputs(A,B,C,D,countPhase, out);
+	input A,B,C,D;
+	input [3:0] countPhase;
+	output out;
+	not(NcountPhase[1], countPhase[1]);
+	not(NcountPhase[3], countPhase[3]);
+	
+	and_gate_4_inputs gate_1(.A(countPhase[0]),.B(Ninput_sel[1]),.C(countPhase[2]),.D(Ninput_sel[3]),.S(out),);
