f484dac45769743609ee84ace04add1d807f6820
BRCZ1N <brunocamposo.r@gmail.com>
2024-01-12 16:11:39 -0300

    Update modulo_valor_minimo.v

--- modulo_valor_minimo.v
+++ modulo_valor_minimo.v
@@ -1,9 +1,7 @@
 module modulo_valor_minimo(REG, MIN);
 
-
 input[6:0]REG;
 output MIN;
-
 
 not(NREG[6], REG[6]);
 
@@ -15,6 +13,6 @@ not(NREG[3], REG[3]);
 
 not(NREG[1], REG[1]);
 
+and_gate_7_inputs gate_1(.A(NREG[6]),.B(NREG[5]),.C(NREG[4]),.D(NREG[3]),.E(NREG[2]),.F(NREG[1]),.G(NREG[0]),.S(MIN));
 
-
-and_gate_7_inputs gate_1(.A(NREG[6]),.B(NREG[5]),.C(NREG[4]),.D(NREG[3]),.E(NREG[2]),.F(NREG[1]),.G(NREG[0]),.S(MIN),);
+endmodule 
