b5165d2a4b27e13f590ba05d64d7c84cf7386793
BRCZ1N <brunocamposo.r@gmail.com>
2024-01-16 14:44:46 -0300

    Revert "Merge pull request #10 from BRCZ1N/modulo_valor_minimo"
    This reverts commit 8a05d40801f79956f9ecc65cf04b81b00c892bfc, reversing
    changes made to 3f0a6ea9942bb185fed303e94b44245570c72dec.

--- modulo_valor_minimo.v
+++ modulo_valor_minimo.v
@@ -3,7 +3,6 @@ module modulo_valor_minimo(REG, MIN);
 input [6:0]REG;
 output MIN;
 wire [6:0] NREG;
-wire [1:0] ands;
 
 not(NREG[6], REG[6]);
 
@@ -15,13 +14,6 @@ not(NREG[3], REG[3]);
 
 not(NREG[1], REG[1]);
 
-and_gate_5_inputs gate_1(.A(NREG[6]),.B(NREG[5]),.C(NREG[4]),.D(NREG[3]),.E(NREG[2]),.S(ands[0]));
-
-
-and_gate_5_inputs gate_2(.A(NREG[6]),.B(NREG[5]),.C(NREG[4]),.D(NREG[3]),.E(NREG[1]),.S(ands[1]));
-
-
-or_gate_2_inputs gate_3(.A(ands[0]),.B(ands[1]),.S(MIN));
-
+and_gate_7_inputs gate_1(.A(NREG[6]),.B(NREG[5]),.C(NREG[4]),.D(NREG[3]),.E(NREG[2]),.F(NREG[1]),.G(NREG[0]),.S(MIN));
 
 endmodule 
