1233223a488f71f17fa35d3be26aceb8f88dce4c
BRCZ1N <brunocamposo.r@gmail.com>
2024-01-17 15:25:07 -0300

    Arrumando o codificador do display de unidade para rolhas

--- modulo_codificador_dezena_rolhas.v
+++ modulo_codificador_dezena_rolhas.v
@@ -1,60 +1,60 @@
-module modulo_codificador_dezena_rolhas(abcd, s);
+module modulo_codificador_dezena_rolhas(REG_R, REG_RD);
 
-input [6:0] abcd;
-output [3:0] s;
+input [6:0] REG_R;
+output [3:0] REG_RD;
 
-wire [6:0] Nabcd;
+wire [6:0] NREG_R;
 
-wire [1:0] aux_a;
-wire [2:0] aux_b;
-wire [4:0] aux_c;
-wire [11:0] aux_d;
+wire [1:0] aux_reg_rd1;
+wire [2:0] aux_reg_rd2;
+wire [4:0] aux_reg_rd3;
+wire [11:0] aux_reg_rd4;
 
-not (Nabcd[6], abcd[6]);
-not (Nabcd[5], abcd[5]);
-not (Nabcd[4], abcd[4]);
-not (Nabcd[3], abcd[3]);
-not (Nabcd[2], abcd[2]);
-not (Nabcd[1], abcd[1]);
-not (Nabcd[0], abcd[0]);
+not (NREG_R[6], REG_R[6]);
+not (NREG_R[5], REG_R[5]);
+not (NREG_R[4], REG_R[4]);
+not (NREG_R[3], REG_R[3]);
+not (NREG_R[2], REG_R[2]);
+not (NREG_R[1], REG_R[1]);
+not (NREG_R[0], REG_R[0]);
 
-//s1 = ac + ab
-and (aux_a[1], abcd[6], abcd[4]);
-and (aux_a[0], abcd[6], abcd[5]);
+//REG_RD1 = ac + ab
+and (aux_reg_rd1[1], REG_R[6], REG_R[4]);
+and (aux_reg_rd1[0], REG_R[6], REG_R[5]);
 
-or (s[3], aux_a[1], aux_a[0]);
+or (REG_RD[3], aux_reg_rd1[1], aux_reg_rd1[0]);
 
-//s2 = bd + bc + ab'c'
-and (aux_b[2], abcd[5], abcd[3]);
-and (aux_b[1], abcd[5], abcd[4]);
-and (aux_b[0], abcd[6], Nabcd[5], Nabcd[4]);
+//REG_RD2 = bd + bc + ab'c'
+and (aux_reg_rd2[2], REG_R[5], REG_R[3]);
+and (aux_reg_rd2[1], REG_R[5], REG_R[4]);
+and (aux_reg_rd2[0], REG_R[6], NREG_R[5], NREG_R[4]);
 
-or (s[2], aux_b[2], aux_b[1], aux_b[0]);
+or (REG_RD[2], aux_reg_rd2[2], aux_reg_rd2[1], aux_reg_rd2[0]);
 
-//s3 = a'b'ce + a'b'cd + a'bc'd' + ab'c' + a'cde
-and (aux_c[4], Nabcd[6], Nabcd[5], abcd[4], abcd[2]);
-and (aux_c[3], Nabcd[6], Nabcd[5], abcd[4], abcd[3]);
-and (aux_c[2], Nabcd[6], abcd[5], Nabcd[4], Nabcd[3]);
-and (aux_c[1], abcd[6], Nabcd[5], Nabcd[4]);
-and (aux_c[0], Nabcd[6], abcd[4], abcd[3], abcd[2]);
+//REG_RD3 = a'b'ce + a'b'cd + a'bc'd' + ab'c' + a'cde
+and (aux_reg_rd3[4], NREG_R[6], NREG_R[5], REG_R[4], REG_R[2]);
+and (aux_reg_rd3[3], NREG_R[6], NREG_R[5], REG_R[4], REG_R[3]);
+and (aux_reg_rd3[2], NREG_R[6], REG_R[5], NREG_R[4], NREG_R[3]);
+and (aux_reg_rd3[1], REG_R[6], NREG_R[5], NREG_R[4]);
+and (aux_reg_rd3[0], NREG_R[6], REG_R[4], REG_R[3], REG_R[2]);
 
-or (s[1], aux_c[4], aux_c[3], aux_c[2], aux_c[1], aux_c[0]);
+or (REG_RD[1], aux_reg_rd3[4], aux_reg_rd3[3], aux_reg_rd3[2], aux_reg_rd3[1], aux_reg_rd3[0]);
 
-//s4 = b'c'df + b'c'de + a'b'cd'e' + b'def + bc'd' + bd'e + bcde' + ac'ef + ac'd + adf + ade + a'cd'e'f 
-and (aux_d[11], Nabcd[5], Nabcd[4], abcd[3], abcd[1]);
-and (aux_d[10], Nabcd[5], Nabcd[4], abcd[3], abcd[2]);
-and (aux_d[9], Nabcd[6], Nabcd[5], abcd[4], Nabcd[3], Nabcd[2]);
-and (aux_d[8], Nabcd[5], abcd[3], abcd[2], abcd[1]);
-and (aux_d[7], abcd[5], Nabcd[4], Nabcd[3]);
-and (aux_d[6], abcd[5], Nabcd[3], abcd[2]);
-and (aux_d[5], abcd[5], abcd[4], abcd[3], Nabcd[2]);
-and (aux_d[4], abcd[6], Nabcd[4], abcd[2], abcd[1]);
-and (aux_d[3], abcd[6], Nabcd[4], abcd[3]);
-and (aux_d[2], abcd[6], abcd[3], abcd[1]);
-and (aux_d[1], abcd[6], abcd[3], abcd[2]);
-and (aux_d[0], Nabcd[6], abcd[4], Nabcd[3], Nabcd[2], abcd[1]);
+//REG_RD4 = b'c'df + b'c'de + a'b'cd'e' + b'def + bc'd' + bd'e + bcde' + ac'ef + ac'd + adf + ade + a'cd'e'f 
+and (aux_reg_rd4[11], NREG_R[5], NREG_R[4], REG_R[3], REG_R[1]);
+and (aux_reg_rd4[10], NREG_R[5], NREG_R[4], REG_R[3], REG_R[2]);
+and (aux_reg_rd4[9], NREG_R[6], NREG_R[5], REG_R[4], NREG_R[3], NREG_R[2]);
+and (aux_reg_rd4[8], NREG_R[5], REG_R[3], REG_R[2], REG_R[1]);
+and (aux_reg_rd4[7], REG_R[5], NREG_R[4], NREG_R[3]);
+and (aux_reg_rd4[6], REG_R[5], NREG_R[3], REG_R[2]);
+and (aux_reg_rd4[5], REG_R[5], REG_R[4], REG_R[3], NREG_R[2]);
+and (aux_reg_rd4[4], REG_R[6], NREG_R[4], REG_R[2], REG_R[1]);
+and (aux_reg_rd4[3], REG_R[6], NREG_R[4], REG_R[3]);
+and (aux_reg_rd4[2], REG_R[6], REG_R[3], REG_R[1]);
+and (aux_reg_rd4[1], REG_R[6], REG_R[3], REG_R[2]);
+and (aux_reg_rd4[0], NREG_R[6], REG_R[4], NREG_R[3], NREG_R[2], REG_R[1]);
 
-or (s[0], aux_d[11], aux_d[10], aux_d[9], aux_d[8], aux_d[7], aux_d[6], aux_d[5], aux_d[4], aux_d[3], aux_d[2], aux_d[1], aux_d[0]);
+or (REG_RD[0], aux_reg_rd4[11], aux_reg_rd4[10], aux_reg_rd4[9], aux_reg_rd4[8], aux_reg_rd4[7], aux_reg_rd4[6], aux_reg_rd4[5], aux_reg_rd4[4], aux_reg_rd4[3], aux_reg_rd4[2], aux_reg_rd4[1], aux_reg_rd4[0]);
 
 
 endmodule
--- modulo_codificador_unidade_rolhas.v
+++ modulo_codificador_unidade_rolhas.v
@@ -1,69 +1,74 @@
-module modulo_codificador_unidade_rolhas(int, s);
-input [6:0] int;
-output  [3:0]s;
+module modulo_codificador_unidade_rolhas(REG_R, REG_RU);
+input [6:0] REG_R;
+output [3:0] REG_RU;
 
-wire [6:0] Nint;
+wire [6:0] NREG_R;
 
-wire [12:0] aux_a;
-wire [12:0] aux_b;
-wire [9:0] aux_c;
+wire [9:0] aux_reg_ru_1;
+wire [12:0] aux_reg_ru_2;
+wire [12:0] aux_reg_ru_3;
 
-not (Nint[6], int[6]);
-not (Nint[5], int[5]);
-not (Nint[4], int[4]);
-not (Nint[3], int[3]);
-not (Nint[2], int[2]);
-not (Nint[1], int[1]);
-not (Nint[0], int[0]);
+not (NREG_R[6], REG_R[6]);
+not (NREG_R[5], REG_R[5]);
+not (NREG_R[4], REG_R[4]);
+not (NREG_R[3], REG_R[3]);
+not (NREG_R[2], REG_R[2]);
+not (NREG_R[1], REG_R[1]);
+not (NREG_R[0], REG_R[0]);
 
+//REG_RU1 = a'b'c'de'f' + a'b'cd'e'f + a'b'cdef' + bc'd'ef + bcd'e'f' + bcde'f + ac'd'ef' + ac'def + acde'f' + abf
+and (aux_reg_ru_1[9], NREG_R[6], NREG_R[5], NREG_R[4], REG_R[3], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_1[8], NREG_R[6], NREG_R[5], REG_R[4], NREG_R[3], NREG_R[2], REG_R[1]);
+and (aux_reg_ru_1[7], NREG_R[6], NREG_R[5], REG_R[4], REG_R[3], REG_R[2], NREG_R[1]);
+and (aux_reg_ru_1[6], REG_R[5], NREG_R[4], NREG_R[3], REG_R[2], REG_R[1]);
+and (aux_reg_ru_1[5], REG_R[5], REG_R[4], NREG_R[3], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_1[4], REG_R[5], REG_R[4], REG_R[3], NREG_R[2], REG_R[1]);
+and (aux_reg_ru_1[3], REG_R[6], NREG_R[4], NREG_R[3], REG_R[2], NREG_R[1]);
+and (aux_reg_ru_1[2], REG_R[6], NREG_R[4], REG_R[3], REG_R[2], REG_R[1]);
+and (aux_reg_ru_1[1], REG_R[6], REG_R[4], REG_R[3], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_1[0], REG_R[6], REG_R[5], REG_R[1]);
 
-//S3 = a'b'ce'f' + a'b'cde' + a'bc'd'e'f + bcd'ef + ac'def' + acd'e + acef + a'b'c'd'e + a'c'def + bc'ef' + a'cde'f' + ac'd'e'f' + ab'c'e'f
-and (aux_a[12], Nint[6], Nint[5], int[4], Nint[2], Nint[1]);
-and (aux_a[11], Nint[6], Nint[5], int[4], int[3], Nint[2]);
-and (aux_a[10], Nint[6], int[5], Nint[4], Nint[3], Nint[2], int[1]);
-and (aux_a[9], int[5], int[4], Nint[3], int[2], int[1]);
-and (aux_a[8], int[6], Nint[4], int[3], int[2], Nint[1]);
-and (aux_a[7], int[6], int[4], Nint[3], int[2]);
-and (aux_a[6], int[6], int[4], int[2], int[1]);
-and (aux_a[5], Nint[6], Nint[5], Nint[4], Nint[3], int[2]);
-and (aux_a[4], Nint[6], Nint[4], int[3], int[2], int[1]);
-and (aux_a[3], int[5], Nint[4], int[2], Nint[1]);
-and (aux_a[2], Nint[6], int[4], int[3], Nint[2], Nint[1]);
-and (aux_a[1], int[6], Nint[4], Nint[3], Nint[2], Nint[1]);
-and (aux_a[0], int[6], Nint[5], Nint[4], Nint[2], int[1]);
-or (s[3], aux_a[12], aux_a[11], aux_a[10], aux_a[9], aux_a[8], aux_a[7], aux_a[6], aux_a[5], aux_a[4], aux_a[3], aux_a[2], aux_a[1], aux_a[0]);
+or (REG_RU[3],aux_reg_ru_1[9], aux_reg_ru_1[8], aux_reg_ru_1[7], aux_reg_ru_1[6], aux_reg_ru_1[5], aux_reg_ru_1[4], aux_reg_ru_1[3], aux_reg_ru_1[2], aux_reg_ru_1[1], aux_reg_ru_1[0]);
 
-//S2 = b'c'def' + a'b'cd'e'f' + a'b'cde'f + bc'd'f' + bc'df + bd'ef' + bcde'f' + bdef + ac'df' + adef' + a'b'c'd'f + b'cd'ef + ab'd'e'f
-and (aux_b[12], Nint[5], Nint[4], int[3], int[2], Nint[1]);
-and (aux_b[11], Nint[6], Nint[5], int[4], Nint[3], Nint[2], Nint[1]);
-and (aux_b[10], Nint[6], Nint[5], int[4], int[3], Nint[2], int[1]);
-and (aux_b[9], int[5], Nint[4], Nint[3], Nint[1]);
-and (aux_b[8], int[5], Nint[4], int[3], int[1]);
-and (aux_b[7], int[5], Nint[3], int[2], Nint[1]);
-and (aux_b[6], int[5], int[4], int[3], Nint[2], Nint[1]);
-and (aux_b[5], int[5], int[3], int[2], int[1]);
-and (aux_b[4], int[6], Nint[4], int[3], Nint[1]);
-and (aux_b[3], int[6], int[3], int[2], Nint[1]);
-and (aux_b[2], Nint[6], Nint[5], Nint[4], Nint[3], int[1]);
-and (aux_b[1], Nint[5], int[4], Nint[3], int[2], int[1]);
-and (aux_b[0], int[6], Nint[5], Nint[3], Nint[2], int[1]);
-or (s[2], aux_b[12], aux_b[11], aux_b[10], aux_b[9], aux_b[8], aux_b[7], aux_b[6], aux_b[5], aux_b[4], aux_b[3], aux_b[2], aux_b[1], aux_b[0]);
+//REG_RU2 = a'b'ce'f' + a'b'cde' + a'bc'd'e'f + bcd'ef + ac'def' + acd'e + acef + a'b'c'd'e + a'c'def + bc'ef' + a'cde'f' + ac'd'e'f' + ab'c'e'f
+and (aux_reg_ru_2[12], NREG_R[6], NREG_R[5], REG_R[4], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_2[11], NREG_R[6], NREG_R[5], REG_R[4], REG_R[3], NREG_R[2]);
+and (aux_reg_ru_2[10], NREG_R[6], REG_R[5], NREG_R[4], NREG_R[3], NREG_R[2], REG_R[1]);
+and (aux_reg_ru_2[9], REG_R[5], REG_R[4], NREG_R[3], REG_R[2], REG_R[1]);
+and (aux_reg_ru_2[8], REG_R[6], NREG_R[4], REG_R[3], REG_R[2], NREG_R[1]);
+and (aux_reg_ru_2[7], REG_R[6], REG_R[4], NREG_R[3], REG_R[2]);
+and (aux_reg_ru_2[6], REG_R[6], REG_R[4], REG_R[2], REG_R[1]);
+and (aux_reg_ru_2[5], NREG_R[6], NREG_R[5], NREG_R[4], NREG_R[3], REG_R[2]);
+and (aux_reg_ru_2[4], NREG_R[6], NREG_R[4], REG_R[3], REG_R[2], REG_R[1]);
+and (aux_reg_ru_2[3], REG_R[5], NREG_R[4], REG_R[2], NREG_R[1]);
+and (aux_reg_ru_2[2], NREG_R[6], REG_R[4], REG_R[3], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_2[1], REG_R[6], NREG_R[4], NREG_R[3], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_2[0], REG_R[6], NREG_R[5], NREG_R[4], NREG_R[2], REG_R[1]);
 
-//S1 = a'b'c'de'f' + a'b'cd'e'f + a'b'cdef' + bc'd'ef + bcd'e'f' + bcde'f + ac'd'ef' + ac'def + acde'f' + abf
-and (aux_c[9], Nint[6], Nint[5], Nint[4], int[3], Nint[2], Nint[1]);
-and (aux_c[8], Nint[6], Nint[5], int[4], Nint[3], Nint[2], int[1]);
-and (aux_c[7], Nint[6], Nint[5], int[4], int[3], int[2], Nint[1]);
-and (aux_c[6], int[5], Nint[4], Nint[3], int[2], int[1]);
-and (aux_c[5], int[5], int[4], Nint[3], Nint[2], Nint[1]);
-and (aux_c[4], int[5], int[4], int[3], Nint[2], int[1]);
-and (aux_c[3], int[6], Nint[4], Nint[3], int[2], Nint[1]);
-and (aux_c[2], int[6], Nint[4], int[3], int[2], int[1]);
-and (aux_c[1], int[6], int[4], int[3], Nint[2], Nint[1]);
-and (aux_c[0], int[6], int[5], int[1]);
-or (s[1], aux_c[9], aux_c[8], aux_c[7], aux_c[6], aux_c[5], aux_c[4], aux_c[3], aux_c[2], aux_c[1], aux_c[0]);
+or (REG_RU[2], aux_reg_ru_2[12], aux_reg_ru_2[11], aux_reg_ru_2[10], aux_reg_ru_2[9], aux_reg_ru_2[8], aux_reg_ru_2[7], aux_reg_ru_2[6], aux_reg_ru_2[5], aux_reg_ru_2[4], aux_reg_ru_2[3], aux_reg_ru_2[2], aux_reg_ru_2[1], aux_reg_ru_2[0]);
 
-//S0 = g
-and (s[0], int[0]);
+//REG_RU3 = b'c'def' + a'b'cd'e'f' + a'b'cde'f + bc'd'f' + bc'df + bd'ef' + bcde'f' + bdef + ac'df' + adef' + a'b'c'd'f + b'cd'ef + ab'd'e'f
+
+and (aux_reg_ru_3[12], NREG_R[5], NREG_R[4], REG_R[3], REG_R[2], NREG_R[1]);
+and (aux_reg_ru_3[11], NREG_R[6], NREG_R[5], REG_R[4], NREG_R[3], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_3[10], NREG_R[6], NREG_R[5], REG_R[4], REG_R[3], NREG_R[2], REG_R[1]);
+and (aux_reg_ru_3[9], REG_R[5], NREG_R[4], NREG_R[3], NREG_R[1]);
+and (aux_reg_ru_3[8], REG_R[5], NREG_R[4], REG_R[3], REG_R[1]);
+and (aux_reg_ru_3[7], REG_R[5], NREG_R[3], REG_R[2], NREG_R[1]);
+and (aux_reg_ru_3[6], REG_R[5], REG_R[4], REG_R[3], NREG_R[2], NREG_R[1]);
+and (aux_reg_ru_3[5], REG_R[5], REG_R[3], REG_R[2], REG_R[1]);
+and (aux_reg_ru_3[4], REG_R[6], NREG_R[4], REG_R[3], NREG_R[1]);
+and (aux_reg_ru_3[3], REG_R[6], REG_R[3], REG_R[2], NREG_R[1]);
+and (aux_reg_ru_3[2], NREG_R[6], NREG_R[5], NREG_R[4], NREG_R[3], REG_R[1]);
+and (aux_reg_ru_3[1], NREG_R[5], REG_R[4], NREG_R[3], REG_R[2], REG_R[1]);
+and (aux_reg_ru_3[0], REG_R[6], NREG_R[5], NREG_R[3], NREG_R[2], REG_R[1]);
+
+or (REG_RU[1], aux_reg_ru_3[12], aux_reg_ru_3[11], aux_reg_ru_3[10], aux_reg_ru_3[9], aux_reg_ru_3[8], aux_reg_ru_3[7], aux_reg_ru_3[6], aux_reg_ru_3[5], aux_reg_ru_3[4], aux_reg_ru_3[3], aux_reg_ru_3[2], aux_reg_ru_3[1], aux_reg_ru_3[0]);
+
+//REG_RU4 = g
+
+and (REG_RU[0], REG_R[0], 1'b1);
+
 
 endmodule 
 
