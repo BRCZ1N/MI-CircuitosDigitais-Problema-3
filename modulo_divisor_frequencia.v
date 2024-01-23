a391db41448e9ccaee3d5ee4820c6d35f1a4faaa
BRCZ1N <brunocamposo.r@gmail.com>
2024-01-23 01:17:40 -0300

    Revert "Alterações no buffer principal/secundário"
    This reverts commit a8e7a840dfe6a511ca009cd810dd83fa71ede8b6.

--- modulo_divisor_frequencia.v
+++ modulo_divisor_frequencia.v
@@ -9,79 +9,79 @@ module modulo_divisor_frequencia(preset, clear, clock, clock_div);
 	and(clock_div,1'b1,q[18]);
 
 	
-	modulo_ff_t ff_1(.t(1'b1),.clock(clock),.preset(preset),.clear(clear),.q(q[0]),.q_bar());
+	modulo_ff_t ff_1(.t(1'b1),.clock(clock),.preset(1'b0),.clear(1'b0),.q(q[0]),.q_bar());
 	
 	// F/2
 	
-	modulo_ff_t ff_2(.t(1'b1),.clock(q[0]),.preset(preset),.clear(clear),.q(q[1]),.q_bar()); 
+	modulo_ff_t ff_2(.t(1'b1),.clock(q[0]),.preset(1'b0),.clear(1'b0),.q(q[1]),.q_bar()); 
 	
 	// F/4
 	
-	modulo_ff_t ff_3(.t(1'b1),.clock(q[1]),.preset(preset),.clear(clear),.q(q[2]),.q_bar()); 
+	modulo_ff_t ff_3(.t(1'b1),.clock(q[1]),.preset(1'b0),.clear(1'b0),.q(q[2]),.q_bar()); 
 	
 	// F/8
 	
-	modulo_ff_t ff_4(.t(1'b1),.clock(q[2]),.preset(preset),.clear(clear),.q(q[3]),.q_bar()); 
+	modulo_ff_t ff_4(.t(1'b1),.clock(q[2]),.preset(1'b0),.clear(1'b0),.q(q[3]),.q_bar()); 
 	
 	// F/16
 	
-	modulo_ff_t ff_5(.t(1'b1),.clock(q[3]),.preset(preset),.clear(clear),.q(q[4]),.q_bar()); 
+	modulo_ff_t ff_5(.t(1'b1),.clock(q[3]),.preset(1'b0),.clear(1'b0),.q(q[4]),.q_bar()); 
 	
 	// F/32
 	
-	modulo_ff_t ff_6(.t(1'b1),.clock(q[4]),.preset(preset),.clear(clear),.q(q[5]),.q_bar()); 
+	modulo_ff_t ff_6(.t(1'b1),.clock(q[4]),.preset(1'b0),.clear(1'b0),.q(q[5]),.q_bar()); 
 	
 	// F/64
 	
-	modulo_ff_t ff_7(.t(1'b1),.clock(q[5]),.preset(preset),.clear(clear),.q(q[6]),.q_bar()); 
+	modulo_ff_t ff_7(.t(1'b1),.clock(q[5]),.preset(1'b0),.clear(1'b0),.q(q[6]),.q_bar()); 
 	
 	// F/128
 	
-	modulo_ff_t ff_8(.t(1'b1),.clock(q[6]),.preset(preset),.clear(clear),.q(q[7]),.q_bar());
+	modulo_ff_t ff_8(.t(1'b1),.clock(q[6]),.preset(1'b0),.clear(1'b0),.q(q[7]),.q_bar());
 																					  
 	// F/256
 	
-	modulo_ff_t ff_9(.t(1'b1),.clock(q[7]),.preset(preset),.clear(clear),.q(q[8]),.q_bar());
+	modulo_ff_t ff_9(.t(1'b1),.clock(q[7]),.preset(1'b0),.clear(1'b0),.q(q[8]),.q_bar());
 																					  
 	// F/512
 	
-	modulo_ff_t ff_10(.t(1'b1),.clock(q[8]),.preset(preset),.clear(clear),.q(q[9]),.q_bar());
+	modulo_ff_t ff_10(.t(1'b1),.clock(q[8]),.preset(1'b0),.clear(1'b0),.q(q[9]),.q_bar());
 																					  
 	// F/1024
 	
-	modulo_ff_t ff_11(.t(1'b1),.clock(q[9]),.preset(preset),.clear(clear),.q(q[10]),.q_bar());
+	modulo_ff_t ff_11(.t(1'b1),.clock(q[9]),.preset(1'b0),.clear(1'b0),.q(q[10]),.q_bar());
 				
 	// F/2048
 	
-	modulo_ff_t ff_12(.t(1'b1),.clock(q[10]),.preset(preset),.clear(clear),.q(q[11]),.q_bar());
+	modulo_ff_t ff_12(.t(1'b1),.clock(q[10]),.preset(1'b0),.clear(1'b0),.q(q[11]),.q_bar());
 																					  
 	// F/4096
 	
-	modulo_ff_t ff_13(.t(1'b1),.clock(q[11]),.preset(preset),.clear(clear),.q(q[12]),.q_bar());
+	modulo_ff_t ff_13(.t(1'b1),.clock(q[11]),.preset(1'b0),.clear(1'b0),.q(q[12]),.q_bar());
 																					  
 	// F/8192
 	
-	modulo_ff_t ff_14(.t(1'b1),.clock(q[12]),.preset(preset),.clear(clear),.q(q[13]),.q_bar());
+	modulo_ff_t ff_14(.t(1'b1),.clock(q[12]),.preset(1'b0),.clear(1'b0),.q(q[13]),.q_bar());
 			
 	// F/16384
 	
-	modulo_ff_t ff_15(.t(1'b1),.clock(q[13]),.preset(preset),.clear(clear),.q(q[14]),.q_bar());
+	modulo_ff_t ff_15(.t(1'b1),.clock(q[13]),.preset(1'b0),.clear(1'b0),.q(q[14]),.q_bar());
 					
 	// F/32768					
 	
-	modulo_ff_t ff_16(.t(1'b1),.clock(q[14]),.preset(preset),.clear(clear),.q(q[15]),.q_bar());
+	modulo_ff_t ff_16(.t(1'b1),.clock(q[14]),.preset(1'b0),.clear(1'b0),.q(q[15]),.q_bar());
 					
 	// F/65536
 
-	modulo_ff_t ff_17(.t(1'b1),.clock(q[15]),.preset(preset),.clear(clear),.q(q[16]),.q_bar());
+	modulo_ff_t ff_17(.t(1'b1),.clock(q[15]),.preset(1'b0),.clear(1'b0),.q(q[16]),.q_bar());
 					
 	// F/131072
 	
-	modulo_ff_t ff_18(.t(1'b1),.clock(q[16]),.preset(preset),.clear(clear),.q(q[17]),.q_bar());
+	modulo_ff_t ff_18(.t(1'b1),.clock(q[16]),.preset(1'b0),.clear(1'b0),.q(q[17]),.q_bar());
 					
 	// F/262144
 	
-	modulo_ff_t ff_19(.t(1'b1),.clock(q[17]),.preset(preset),.clear(clear),.q(q[18]),.q_bar());
+	modulo_ff_t ff_19(.t(1'b1),.clock(q[17]),.preset(1'b0),.clear(1'b0),.q(q[18]),.q_bar());
 				
 	// F/524288
 	
--- pbl.v
+++ pbl.v
@@ -28,7 +28,7 @@ module pbl(start_stop,pg,ch,cq,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nout_7seg,N
 	not(Nout_7seg[1],out_7seg[1]);
 	not(Nout_7seg[0],out_7seg[0]);
 	
-	modulo_divisor_frequencia divisor_f(.preset(1'b0),.clear(1'b0),.clock(clock_50mhz),.clock_div(clk_div));
+	modulo_divisor_frequencia divisor_f(.clk(clock_50mhz),.clr(1'b0),.clk_div(clk_div));
 	
 	modulo_mef_enchimento_vedacao mef_1(.enable(start_stop),.pg(pg),.ch(ch),.ro(ro),.eb(sinal_duzias_reset),.clk(clk_div),.m(m),.ve(ve),.al(al),.ev(ev),.q0(mef_estado[0]),.q1(mef_estado[1]));
 	
@@ -36,17 +36,17 @@ module pbl(start_stop,pg,ch,cq,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nout_7seg,N
 	
 	and_gate_2_inputs gate_1(.A(ve),.B(cq),.S(conta_duzias));
 	
-	modulo_contador_sync_4_bits_ascendente contador_duzias(.input_primeiro_ff(conta_duzias),.preset(1'b0),.clear(sinal_duzias_reset),.clock(clk_div),.q(out_4_bits_duzias));
+	modulo_contador_sync_4_bits contador_duzias(.input_primeiro_ff(conta_duzias),.clr(sinal_duzias_reset),.clk(clk_div),.q(out_4_bits_duzias));
 	modulo_reset_contador_d reset_1(.cd(out_4_bits_duzias), .rst_cd(sinal_duzias_reset));
 	
-	modulo_contador_sync_4_bits_ascendente contador_dezenas_duzias(.input_primeiro_ff(sinal_duzias_reset),.preset(1'b0),.clear(sinal_dezenas_duzias_reset),.clock(clk_div),.q(out_4_bits_dezena_duzias));
+	modulo_contador_sync_4_bits contador_dezenas_duzias(.input_primeiro_ff(sinal_duzias_reset),.clr(sinal_dezenas_duzias_reset),.clk(clk_div),.q(out_4_bits_dezena_duzias));
 	modulo_reset_contador_dd reset_2(.cdd(out_4_bits_dezena_duzias), .rst_cdd(sinal_dezenas_duzias_reset_aux));
 	
 	or_gate_2_inputs gate_2(.A(start_stop),.B(sinal_dezenas_duzias_reset_aux),.S(sinal_dezenas_duzias_reset));
 	
 	//Circuito de contagem de rolhas
 	
-	modulo_contador_sync_7_bits_ascendente contador_entrada_rolhas(.preset(1'b0),.clear(1'b0),.clock(op_c_deboucing),.q(rolhas_entrada));
+	modulo_contador_sync_7_bits contador_entrada_rolhas(.clr(1'b0),.clk(op_c_deboucing),.q(rolhas_entrada));
 	
 	modulo_seletor_permissoes seletor_1(.ve(ve),.min_r(signal_min_rolhas),.op(op_deboucing),.sel_perm(sel_mux_e));
 	
@@ -74,7 +74,7 @@ module pbl(start_stop,pg,ch,cq,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nout_7seg,N
 	
 	modulo_verificador_ausencia_rolhas m_aus_rolhas(.reg_r(buffer_saida), .aus_rolhas(ro));
 	
-	modulo_contador_sync_2_bits_ascendente contador_display(.clear(1'b0),.clock(clk_div),.q(sel_mux_display));
+	modulo_contador_sync_2_bits contador_display(.clr(1'b0),.clk(clk_div),.q(sel_mux_display));
 	
 	modulo_codificador_dezena_garrafas codificador_garrafas_1(.cdd(out_4_bits_dezena_duzias),.cdfd(codificacao_4bits_d_garrafas));
 	modulo_codificador_unidade_garrafas codificador_garrafas_2(.cdd(out_4_bits_dezena_duzias),.cdfu(codificacao_4bits_u_garrafas));
