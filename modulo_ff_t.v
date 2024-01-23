25bdde991e53f5ba75b504a4e19627bbbe9907e2
BRCZ1N <brunocamposo.r@gmail.com>
2024-01-23 01:17:57 -0300

    Revert "Alterações no buffer principal"
    This reverts commit 0972bb53a0130842181ee912b1023ddffd4ed8dc.

--- modulo_ff_t.v
+++ modulo_ff_t.v
@@ -1,30 +1,40 @@
-// Finalizado
-module modulo_ff_t(
-    input t, 
-    input clock,  
-    input preset, 
-    input clear, 
-    output reg q,
-    output reg q_bar
-);
+//Finalizado
+module modulo_ff_t(t,clk,clr,q);
 
-always @(negedge clock or posedge clear or posedge preset) 
-begin 
-    if (clear == 1'b1)
-    begin
-        q <= 1'b0;
-        q_bar <= 1'b1;
-    end
-    else if (preset == 1'b1)
-    begin
-        q <= 1'b1;
-        q_bar <= 1'b0;
-    end
-    else if (t)
-    begin
-        q <= !q;
-        q_bar <= !q;
-    end
-end
 
-endmodule
+	input t,clk,clr;
+	output reg q;
+	
+	always @(negedge clk) 
+	
+	begin 
+	
+		if(clr == 1'b1) 
+		
+		begin 
+	
+			q <= 1'b0;
+	
+		end 
+		
+		else if( t )
+		
+		begin
+		
+			q <= !q;
+		
+		end
+		
+		else
+		
+		begin
+		
+			q <= q;
+		
+		end
+		
+	
+	end
+
+
+endmodule 
