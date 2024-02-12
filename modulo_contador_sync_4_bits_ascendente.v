266e5cb46b26141058704c4570722c3ca349669f
BRCZ1N <brunocamposo.r@gmail.com>
2024-02-11 21:24:40 -0300

    Aparentemente funcionando o sistema, falta implementar a atuação do enable no processo completo
    Funcionamento básico do sistema de rolhas operacional , alteração dos reset para nível baixo do sistema de contagem de garrafas. além de alteração nas entradas preset e clear do contador para o display de segmentos

--- modulo_contador_sync_4_bits_ascendente.v
+++ modulo_contador_sync_4_bits_ascendente.v
@@ -4,15 +4,15 @@ module modulo_contador_sync_4_bits_ascendente(input_primeiro_ff, clr, clk, q, pr
   output [3:0] q;
   wire [1:0] ff_inputs;
 
-  modulo_ff_t ff_1(.t(input_primeiro_ff), .clk(clk), .clr(clr), .prst(prst), .q(q[0]));
-  modulo_ff_t ff_2(.t(q[0]), .clk(clk), .clr(clr), .prst(prst), .q(q[1]));
+  modulo_ff_t ff_1(.t(input_primeiro_ff),.clk(clk),.clr(clr),.prst(prst),.q(q[0]) ,.enable(1'b1));
+  modulo_ff_t ff_2(.t(q[0]),.clk(clk),.clr(clr),.prst(prst), .q(q[1]),.enable(1'b1));
 
   and_gate_2_inputs gate_1(.A(q[1]), .B(q[0]), .S(ff_inputs[0]));
 
-  modulo_ff_t ff_3(.t(ff_inputs[0]), .clk(clk), .clr(clr), .prst(prst), .q(q[2]));
+  modulo_ff_t ff_3(.t(ff_inputs[0]),.clk(clk),.clr(clr),.prst(prst),.q(q[2]),.enable(1'b1));
 
   and_gate_2_inputs gate_2(.A(ff_inputs[0]), .B(q[2]), .S(ff_inputs[1]));
 
-  modulo_ff_t ff_4(.t(ff_inputs[1]), .clk(clk), .clr(clr), .prst(prst), .q(q[3]));
+  modulo_ff_t ff_4(.t(ff_inputs[1]),.clk(clk),.clr(clr),.prst(prst),.q(q[3]),.enable(1'b1));
 
 endmodule
--- pbl.v
+++ pbl.v
@@ -6,14 +6,14 @@ module pbl(start_stop,pg,ch,cq,hh_load,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nou
 	output [1:0] mef_estado;
 	output [7:0] Nout_7seg;
 	output [3:0] Nac_7segmentos;
-	wire enable_buffer_principal, Nenable_count_operador, enable_count_min_rolhas_aux, Nve, reset_ff_operador, reset_ff_rolhas, load_count_operador, enable_count_operador, enable_count_min_rolhas, load_count_min_rolhas, enable_buffer_secundario, Nop_c_deboucing; 
-	wire Nout_range_buffer, Nop_deboucing, Nstart_stop, comparator_aux, clk_div, controle_sel_rolhas, controle_sel_operador;
-	wire out_range_buffer, sinal_duzias_reset, conta_duzias, sinal_dezenas_duzias_reset_aux,sinal_dezenas_duzias_reset, signal_min_rolhas, ro, min_trans_rolhas;
+	wire estado_vedacao, enable_buffer_principal, Nenable_count_operador, enable_count_min_rolhas_aux, reset_ff_operador, reset_ff_rolhas, load_count_operador, enable_count_operador, enable_count_min_rolhas, load_count_min_rolhas, enable_buffer_secundario, Nop_c_deboucing; 
+	wire Nestado_vedacao, Nout_range_buffer, Nop_deboucing, Nstart_stop, comparator_aux, clk_div, controle_sel_rolhas, controle_sel_operador;
+	wire Nsinal_dezenas_duzias_reset, Nsinal_duzias_reset, out_range_buffer, sinal_duzias_reset, conta_duzias, sinal_dezenas_duzias_reset_aux,sinal_dezenas_duzias_reset, signal_min_rolhas, ro, min_trans_rolhas;
 	wire [3:0] out_4_bits_duzias,out_4_bits_dezena_duzias,display_in, ac_7segmentos, codificacao_4bits_d_garrafas, codificacao_4bits_u_garrafas, codificacao_4bits_d_rolhas,codificacao_4bits_u_rolhas;
 	wire [6:0] Nbuffer_secundario_controle_operador, Nbuffer_secundario_controle_min_rolhas, buffer_secundario_controle_min_rolhas, buffer_secundario_controle_operador, buffer_principal, buffer_secundario, buffer_secundario_atual_sum_op, buffer_secundario_controle, buffer_secundario_aux, buffer_entrada_aux_secundario_out, rolhas_entrada_secundario;
 	wire [2:0] out_comparador;
 	wire [7:0] out_7seg;
-	wire [1:0] Ncontador_mef_controle_state_operador, Ncontador_mef_controle_state_rolha,load_reg, sel_mux_display, load_aux, load_input_pulse, controle_reset, contador_mef_controle_state_rolha, contador_mef_controle_state_operador, controle_reset_aux_min_rolhas, controle_reset_aux_operador;
+	wire [1:0] Nmef_estado, Ncontador_mef_controle_state_operador, Ncontador_mef_controle_state_rolha,load_reg, sel_mux_display, load_aux, load_input_pulse, controle_reset, contador_mef_controle_state_rolha, contador_mef_controle_state_operador, controle_reset_aux_min_rolhas, controle_reset_aux_operador;
 	wire [6:0] init_rolhas;
 	
 	and(init_rolhas[6],1'b1,1'b0);
@@ -24,8 +24,12 @@ module pbl(start_stop,pg,ch,cq,hh_load,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nou
 	and(init_rolhas[1],1'b1,1'b0);
 	and(init_rolhas[0],1'b1,1'b0);
 	
+	not(Nsinal_duzias_reset,sinal_duzias_reset);
+	not(Nsinal_dezenas_duzias_reset,sinal_dezenas_duzias_reset);
+	not(Nestado_vedacao,estado_vedacao);
+	not(Nmef_estado[1],mef_estado[1]);
+	not(Nmef_estado[0],mef_estado[0]);
 	not(Nenable_count_operador,enable_count_operador);
-	not(Nve,ve);
 	not(Ncontador_mef_controle_state_rolha[1],contador_mef_controle_state_rolha[1]);
 	not(Ncontador_mef_controle_state_rolha[0],contador_mef_controle_state_rolha[0]);
 	not(Ncontador_mef_controle_state_operador[1],contador_mef_controle_state_operador[1]);
@@ -70,10 +74,10 @@ module pbl(start_stop,pg,ch,cq,hh_load,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nou
 	
 	and_gate_2_inputs gate_1(.A(ve),.B(cq),.S(conta_duzias));
 	
-	modulo_contador_sync_4_bits_ascendente contador_duzias(.input_primeiro_ff(conta_duzias),.prst(1'b1),.clr(sinal_duzias_reset),.clk(clk_div),.q(out_4_bits_duzias));
+	modulo_contador_sync_4_bits_ascendente contador_duzias(.input_primeiro_ff(conta_duzias),.prst(1'b1),.clr(Nsinal_duzias_reset),.clk(clk_div),.q(out_4_bits_duzias));
 	modulo_reset_contador_d reset_1(.cd(out_4_bits_duzias), .rst_cd(sinal_duzias_reset));
 	
-	modulo_contador_sync_4_bits_ascendente contador_dezenas_duzias(.input_primeiro_ff(sinal_duzias_reset),.prst(1'b1),.clr(sinal_dezenas_duzias_reset),.clk(clk_div),.q(out_4_bits_dezena_duzias));
+	modulo_contador_sync_4_bits_ascendente contador_dezenas_duzias(.input_primeiro_ff(sinal_duzias_reset),.prst(1'b1),.clr(Nsinal_dezenas_duzias_reset),.clk(clk_div),.q(out_4_bits_dezena_duzias));
 	modulo_reset_contador_dd reset_2(.cdd(out_4_bits_dezena_duzias),.rst_cdd(sinal_dezenas_duzias_reset_aux));
 	
 	or_gate_2_inputs gate_2(.A(start_stop),.B(sinal_dezenas_duzias_reset_aux),.S(sinal_dezenas_duzias_reset));
@@ -101,6 +105,7 @@ module pbl(start_stop,pg,ch,cq,hh_load,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nou
 	output [6:0] test_buffer_secundario = buffer_secundario;
 	output [6:0] test_buffer_secundario_controle_min_rolhas = buffer_secundario_controle_min_rolhas;
 	output [6:0] test_buffer_secundario_controle_operador = buffer_secundario_controle_operador;
+	output [6:0] test_buffer_principal = buffer_principal;
 	
 	and(controle_sel_rolhas, contador_mef_controle_state_rolha[1], Ncontador_mef_controle_state_rolha[0]);
 	and(controle_sel_operador, contador_mef_controle_state_operador[1], Ncontador_mef_controle_state_operador[0]);
@@ -118,7 +123,9 @@ module pbl(start_stop,pg,ch,cq,hh_load,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nou
 	
 	or(enable_buffer_secundario,enable_count_min_rolhas,enable_count_operador);
 	
-	and(enable_count_min_rolhas,Nve,Nenable_count_operador,enable_count_min_rolhas_aux);
+	and(estado_vedacao, mef_estado[1], mef_estado[0]);
+	
+	and(enable_count_min_rolhas,Nestado_vedacao,Nenable_count_operador,enable_count_min_rolhas_aux);
 	
 	modulo_mef_controle_contador(.enable(1'b1),.Load_Reg(load_reg[0]),.EmptyBuffer(controle_reset[0]),.clk(clk_div),.Enable_C(enable_count_min_rolhas_aux),.Load_C(load_count_min_rolhas),.q0(contador_mef_controle_state_rolha[0]),.q1(contador_mef_controle_state_rolha[1]));
 	modulo_mef_controle_contador(.enable(1'b1),.Load_Reg(load_reg[1]),.EmptyBuffer(controle_reset[1]),.clk(clk_div),.Enable_C(enable_count_operador),.Load_C(load_count_operador),.q0(contador_mef_controle_state_operador[0]),.q1(contador_mef_controle_state_operador[1]));
@@ -127,17 +134,15 @@ module pbl(start_stop,pg,ch,cq,hh_load,clock_50mhz,m,ve,al,Nal,ev,mef_estado,Nou
 	modulo_contador_sync_7_bits_ascendente_descendente contador_2_buffer_secundario_controle_rolhas(.clk(clk_div),.enable(enable_count_min_rolhas),.q(buffer_secundario_controle_min_rolhas),.load(load_count_min_rolhas),.e_load(init_rolhas),.up_down(1'b0));
 	modulo_contador_sync_7_bits_ascendente_descendente contador_3_buffer_secundario_controle_operador(.clk(clk_div),.enable(enable_count_operador),.q(buffer_secundario_controle_operador),.load(load_count_operador),.e_load(buffer_entrada_aux_secundario_out),.up_down(1'b0));
 	
-	or(enable_buffer_principal,ve,enable_count_min_rolhas);
+	or(enable_buffer_principal,estado_vedacao,enable_count_min_rolhas);
 	
-	modulo_contador_sync_7_bits_ascendente_descendente contador_4_buffer_principal(.clk(clk_div),.enable(enable_count_min_rolhas),.q(buffer_principal),.up_down(Nve));
-	
-	output [6:0] test_buffer_principal = buffer_principal;
+	modulo_contador_sync_7_bits_ascendente_descendente contador_4_buffer_principal(.clk(clk_div),.enable(enable_buffer_principal),.q(buffer_principal),.up_down(Nestado_vedacao));
 	
 	modulo_count_superior99 m_out_range(.reg_data(buffer_entrada_aux_secundario_out),.cont_superior_99(out_range_buffer));
 	modulo_valor_transfer_rolhas m_trans_rolha(.reg_r(buffer_secundario),.transfer_rolhas(min_trans_rolhas));
 	modulo_valor_minimo_rolhas m_min_rolhas(.reg_r(buffer_principal),.min_signal(signal_min_rolhas));
 	modulo_verificador_ausencia_rolhas m_aus_rolhas(.reg_r(buffer_principal),.ro(ro));
-	modulo_contador_sync_2_bits_ascendente contador_display(.clr(1'b1),.clk(clk_div),.q(sel_mux_display));
+	modulo_contador_sync_2_bits_ascendente contador_display(.prst(1'b1),.clr(1'b1),.clk(clk_div),.q(sel_mux_display));
 	
 	modulo_codificador_dezena_garrafas codificador_garrafas_1(.cdd(out_4_bits_dezena_duzias),.cdfd(codificacao_4bits_d_garrafas));
 	modulo_codificador_unidade_garrafas codificador_garrafas_2(.cdd(out_4_bits_dezena_duzias),.cdfu(codificacao_4bits_u_garrafas));
